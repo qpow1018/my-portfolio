ea0af48d5e27e9c6230cf28243f78029cfa7a10a
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-03-13 15:37:15 +0900

    Feat: update default style

---
+++ src/style/theme.ts
@@ -0,0 +1,34 @@
+const theme = {
+  color: {
+    primary: '#0072ca',
+    secondary: '#e23e57',
+    text: {
+      primary: '#333',
+      secondary: '#50555c',
+    },
+    // border: {
+    //   default: '#505050',
+    //   light: '#818181',
+    //   dark: '#3c3c3c',
+    // },
+    // dark: {
+    //   gray1: '#1f1f1f',
+    //   gray2: '#292929',
+    //   gray3: '#3c3c3c',
+    //   gray4: '#464646',
+    //   gray5: '#505050',
+    // },
+  },
+  size: {
+  },
+  common: {
+    fontFamily: `'Noto Sans KR', sans-serif`,
+    letterSpacing: '-0.3px',
+    borderRadius: '8px',
+    transition: '0.25s ease',
+  },
+  zIndex: {
+  },
+}
+
+export default theme;
