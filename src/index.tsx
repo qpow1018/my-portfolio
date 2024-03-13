c6a56764de67f62c05bded4b5afbe097c30f6c65
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-03-13 14:51:36 +0900

    Feat: setup default style

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import './style/fonts.css';
+import './style/reset.css';
+import './style/global.css';
+
+import Home from './page/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <Home />
 );
