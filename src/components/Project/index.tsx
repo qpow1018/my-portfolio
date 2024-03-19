32f373b40d4e7cf28ca695931ceee300feb9d860
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-03-19 17:14:24 +0900

    Feat: add Project SectionTitle

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -3,17 +3,70 @@ import theme from '@/style/theme';
 
 export default function Project() {
   return (
-    <Box>
-      <Box>Project</Box>
-      프로젝트 리스트
-      - 이미지 모달
-      - 제목
-      - 소개
-      - 개발 내용
-      - 스킬
-      - 링크
-      
-      UI 구상
+    <Box
+      sx={{
+        padding: '60px 0 100px 0',
+      }}
+    >
+      <Box
+        sx={{
+          margin: '0 auto',
+          width: theme.size.containerWidth,
+        }}
+      >
+        <SectionTitle
+          title={'My Project'}
+        />
+
+        프로젝트 리스트
+        - 이미지 모달
+        - 제목
+        - 소개
+        - 개발 내용
+        - 스킬
+        - 링크
+      </Box>
+
+    </Box>
+  );
+}
+
+function SectionTitle(
+  props: {
+    title: string;
+  }
+) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '40px',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'relative',
+          fontSize: '32px',
+          fontWeight: 700,
+          '&::before, &::after': {
+            content: '""',
+            position: 'absolute',
+            top: 'calc(50% + 2px)',
+            width: '80px',
+            height: '2px',
+            backgroundColor: theme.color.text.primary,
+          },
+          '&::before': {
+            left: '-92px',
+          },
+          '&::after': {
+            right: '-92px',
+          },
+        }}
+      >
+        { props.title }
+      </Box>
     </Box>
   );
 }
