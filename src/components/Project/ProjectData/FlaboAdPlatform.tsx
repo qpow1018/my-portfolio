54846cb39d9600e55b1268b094e9a5002e31ec75
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-03-22 14:46:13 +0900

    Feat: add FlaboAdPlatform content

--- src/components/Project/ProjectData/FlaboAdPlatform.tsx
+++ src/components/Project/ProjectData/FlaboAdPlatform.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 
 import ProjectDataContainer from './ProjectDataContainer';
+import ProjectText from './ProjectText';
 
 export default function FlaboAdPlatform() {
   return (
@@ -13,7 +14,26 @@ export default function FlaboAdPlatform() {
       skills={'React, Typescript, MUI, Axios, Redux'}
       website='비오픈'
     >
+      <Box sx={{ marginBottom: '8px' }}>
+        <ProjectText type='dot'>
+          공통 UI 컴포넌트 개발
+        </ProjectText>
+        <ProjectText type='indent'>
+          - 버튼, 텍스트인풋, 모달, 로딩박스 등
+        </ProjectText>
+      </Box>
 
+      <Box sx={{ marginBottom: '8px' }}>
+        <ProjectText type='dot'>
+          자주 사용되는 색상, 크기 등을 MUI Theme를 이용한 Style 변수 관리
+        </ProjectText>
+      </Box>
+
+      <Box>
+        <ProjectText type='dot'>
+          자주 사용되는 Form Validation을 위한 유효성 검사 모듈 제작
+        </ProjectText>
+      </Box>
     </ProjectDataContainer>
   );
 }
