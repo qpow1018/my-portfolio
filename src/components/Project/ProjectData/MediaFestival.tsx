eed85c9f7af0cd8dc0ed90117c8cfcac3422d0dd
WonjinChoi <qpow1018@gmail.com>
2026-08-03 14:57:08 +0900

    Update media festival portfolio content

--- src/components/Project/ProjectData/MediaFestival.tsx
+++ src/components/Project/ProjectData/MediaFestival.tsx
@@ -12,35 +12,35 @@ export default function MediaFestival() {
     <ProjectDataContainer
       title='2022 1인 미디어 대전'
       description={[
-        '- 행사 소개 및 참가 신청 웹사이트',
+        '- 행사 소개 및 참가 신청 웹사이트 개발',
       ]}
       images={[
         MediaFestivalImage1,
         MediaFestivalImage2,
       ]}
-      skills={'Html, Css, JavaScript'}
+      skills={'HTML, CSS, JavaScript, jQuery'}
     >
       <Box sx={{ marginBottom: '8px' }}>
         <ProjectText type='dot'>
-          CSS 미디어쿼리를 이용하여 반응형 웹 구현
+          CSS 미디어 쿼리를 활용한 반응형 웹 구현
         </ProjectText>
       </Box>
 
       <Box sx={{ marginBottom: '8px' }}>
         <ProjectText type='dot'>
-          Quill 라이브러리를 이용한 게시판 구현
+          Quill 에디터 기반 게시판 구현
         </ProjectText>
       </Box>
 
       <Box sx={{ marginBottom: '8px' }}>
         <ProjectText type='dot'>
-          Jquery - ajax를 이용하여 서버 통신
+          jQuery Ajax를 통한 서버 통신
         </ProjectText>
       </Box>
 
       <Box sx={{ marginBottom: '8px' }}>
         <ProjectText type='dot'>
-          Jquery - load 메소드를 이용하여 공통된 Header, Footer 처리
+          jQuery load를 활용한 공통 Header·Footer 처리
         </ProjectText>
       </Box>
 
@@ -49,16 +49,13 @@ export default function MediaFestival() {
           <Box
             component={'a'}
             target='blank'
-            href='https://www.koreacreatorfesta.co.kr'
+            href='https://creatorfesta.co.kr'
             sx={{
               color: theme.color.primary
             }}
           >
-            https://www.koreacreatorfesta.co.kr
+            https://creatorfesta.co.kr
           </Box>
-        </ProjectText>
-        <ProjectText type='indent'>
-          현재 2023 버전이지만 페이지 구성 및 기능은 동일한걸로 보임
         </ProjectText>
       </Box>
     </ProjectDataContainer>
