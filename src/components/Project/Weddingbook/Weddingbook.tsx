c884b898900e3f07c068d965351b59a85bd5a893
WonjinChoi <qpow1018@gmail.com>
2026-09-09 10:39:57 +0900

    #

--- src/components/Project/Weddingbook/Weddingbook.tsx
+++ src/components/Project/Weddingbook/Weddingbook.tsx
@@ -1,6 +1,7 @@
-import FeatureDevelopment from "./FeatureDevelopment";
-import WebExpansion from "./WebExpansion";
 import WeddingbookHeader from "./WeddingbookHeader";
+import WebExpansion from "./WebExpansion";
+import FeatureDevelopment from "./FeatureDevelopment";
+
 import styles from "./Weddingbook.module.scss";
 
 export default function Weddingbook() {
